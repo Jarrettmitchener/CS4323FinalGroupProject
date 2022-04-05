dc61b089bc3e99d388b3aa0ee9d19d439f2812b0
Jarrettmitchener <77806941+Jarrettmitchener@users.noreply.github.com>
2022-04-05 12:32:27 -0700

    added sofas and waiting room
    using mutex for sofas and semaphore for waiting room, unsure if correct implementation. semaphores could be switched for a mutex this way, also not set to constantly wait for extra sofas nor ignore when waiting room is full

--- fire.c
+++ fire.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <semaphore.h>
+
+#define NUM_OF_SOFAS 7
+#define WAITING_ROOM_SIZE 9
 
 typedef struct _thread_data_t
 {
@@ -11,7 +15,12 @@ typedef struct _thread_data_t
 } thread_data_t;
 
 pthread_mutex_t lock[10];
+pthread_mutex_t sofaLock;
+sem_t roomLock;
+
 int flag[10];
+int sofaCounter = 0;
+int waitingroomCounter = 0;
 
 void initializeMutex()
 {
@@ -30,6 +39,32 @@ void arrInit()
     }
 }
 
+//adds person to sofa
+int addPersonToSofa()
+{
+    if(sofaCounter == NUM_OF_SOFAS)
+    {
+        return 0;
+    }
+    else
+    {
+        sofaCounter++;
+        return 1;
+    }
+}
+
+int addPersonToWaitingRoom()
+{
+    if(waitingroomCounter == WAITING_ROOM_SIZE)
+    {
+        return 0;
+    }
+    else
+    {
+        waitingroomCounter++;
+        return 1;
+    }
+}
 /* thread function */
 void *thr_func(void *arg)
 {
@@ -37,14 +72,41 @@ void *thr_func(void *arg)
     {
         //converts arg to thread data object
         thread_data_t *data = (thread_data_t *)arg;
-        
-        while(flag[data->tid] == 0)//gets stuck until flag is set to something else
+        int id = data->tid;
+        while(flag[id] == 0)//gets stuck until flag is set to something else
         {
             //usleep(5000000);
         }
 
-        printf("hello from thr_func, thread id: %d\n", data->tid);
-        flag[data->tid] = 0;//resets flag
+        printf("hello from thr_func, thread id: %d\n", id);
+
+        //critical section for the semaphore
+        sem_wait(&roomLock);
+        int r = addPersonToWaitingRoom();
+        if(r == 0)
+        {
+            printf("thread ID: %i could not be added to waiting room\n", id);
+        }
+        else
+        {
+            printf("thread ID: %i could be added to waiting room\n", id);
+        }
+        sem_post(&roomLock);
+
+        //critical section begin
+        pthread_mutex_lock(&sofaLock);
+        int temp = addPersonToSofa();
+        if(temp == 0)
+        {
+            printf("thread ID: %i could not be added to sofas\n\n", id);
+        }
+        else
+        {
+            printf("thread ID: %i could be added to sofas\n\n", id);
+        }
+        pthread_mutex_unlock(&sofaLock);
+
+        flag[id] = 0;//resets flag
         pthread_exit(NULL);
     }
  
@@ -55,9 +117,10 @@ int main(int argc, char **argv)
     initializeMutex();
     arrInit();
     
-
     pthread_t thr[10];
     int i, rc;
+    pthread_mutex_init(&sofaLock, NULL);
+    sem_init(&roomLock, 0, 3);//initializes semaphore to 3
     /* create a thread_data_t argument array */
     thread_data_t thr_data[10];
     for (i = 0; i < 10; ++i) {
@@ -70,10 +133,14 @@ int main(int argc, char **argv)
             return EXIT_FAILURE;
         }
     }
-
+    srand(time(NULL));
+    sleep(1);
     for(int i = 0; i < 10; i++)
     {
-        sleep(2);
+        
+        int t = rand() % 5;
+        printf("time value: %i, iteration: %i\n", t, i);
         flag[i] = 1;
+        sleep(2);
     }
 }

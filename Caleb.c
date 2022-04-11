1c1542d9210417ab0e7158f4f21e49cac5f8b9d4
Jarrettmitchener <77806941+Jarrettmitchener@users.noreply.github.com>
2022-04-11 11:38:10 -0700

    Added other team Members files
    Other team members files needed for main.c

---
+++ Caleb.c
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+void makePayment()
+{
+
+}
+
+
+
+void acceptPayment()
+{
+
+}
+
+void leaveClinic()
+{
+
+}
---
+++ Patrick.c
@@ -0,0 +1,16 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+void getMedicalCheckup()
+{
+
+}
+
+void performMedicalCheckup()
+{
+    
+}

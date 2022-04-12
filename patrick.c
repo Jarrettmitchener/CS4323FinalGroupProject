185f2eca930f8fd42a3b5e9195571bfb1553db6e
therick5 <patricklav@protonmail.com>
2022-04-12 16:47:51 -0500

    yes

--- patrick.c
+++ patrick.c
@@ -6,16 +6,19 @@
 
 void GetMedicalCheckup(int *id) {
     sem_wait(&Professional_Ready); // wait for medical professional to be ready
+
     sem_post(&Professional_Ready); // oh boi, he occupied with patient
     printf("Medical Professional %d (Thread ID: %ld): Starting Patient Checkup.\n" , *id, pthread_self());
-    PerformMedicalCheckup(id); // perform it now
+    sem_post(&PerformCheckup);
     printf("Medical Professional %d (Thread ID: %ld): Patient Checkup Complete.\n" , *id, pthread_self());
     sem_post(&Professional_Ready); // he ready
 }
 
 void PerformMedicalCheckup(int *id) {
+    sem_wait(&PerformCheckup);
     printf("Patient %d (Thread ID: %ld): Performing Medical Checkup.\n" , *id, pthread_self());
     sleep(waitTime);
+    sem_pos(&PerformCheckup);
 }
 
 
